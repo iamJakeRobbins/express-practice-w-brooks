a59312d9572071f8915ebf44c8d823d0f78f9b1f
Jake Robbins <jrobbinsd@gmail.com>
2017-12-18 10:47:02 -0700

    site counter

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	req.session.favoriteDog = 'German Shepard';
+
+	if(req.session.count >= 1){
+		req.session.count ++
+	} else {
+		req.session.count = 1
+	}console.log(req.session.count);
   res.render('index', { title: 'Express' });
 });
 
