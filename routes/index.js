49d4a021f83790aa3de137b7add9bb21417a2302
Jake Robbins <jrobbinsd@gmail.com>
2017-12-18 12:17:25 -0700

    added knex seeds and migs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
 	if(req.session.count >= 1){
 		req.session.count ++
 	} else {
---
+++ seeds/01_users.js
@@ -0,0 +1,18 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        {username:
+					 'jake'},
+        {password:
+					 'password1'},
+			 	 {username:
+				 		'david'},
+ 	    	{password:
+							'password2'},
+      ]);
+    });
+};
